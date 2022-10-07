295f647020fbf33d8f6ddc1e2906114d76bc620d
MuhammadHaroonHR <111491015+MuhammadHaroonHR@users.noreply.github.com>
2022-10-07 13:17:28 +0500

    Add files via upload

---
+++ SQEASSIGNMENT.java
@@ -0,0 +1,16 @@
+package lab3task2scd;
+
+public class SQEASSIGNMENT {
+
+	public static void main(String[] args) {
+		int sl;
+		int a = 0;
+		for(int i=0;i<12;i++) {
+			System.out.println(i);
+		}
+		a=a/0;
+		System.out.println(a);
+	
+	}
+
+}
